2be0c75e2176eb75f528fc06c0daec0ac130ed05
Kypo <100861025+KyroWasHere@users.noreply.github.com>
2022-09-05 23:14:06 +0100

    updated

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'progressBars'
+version 'Kyo Retexture'
+
+ui_page '/html/h.html'
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    '/html/h.html',
+    '/html/style.css',
+    '/html/fonts/*.otf'
+}
+
+export "startUI"
